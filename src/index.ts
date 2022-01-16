792ec2043c7e9671bc4c2a20c83d01c6d671cdf8
euclidesdry <euclidesdry@gmail.com>
2022-01-16 17:44:32 +0100

    Creating New User

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express, { Request, Response, NextFunction } from "express";
+import bodyParser from "body-parser";
 
 import userRoutes from './routes/users.route';
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -3,7 +3,7 @@ import statusCode from "http-status-codes";
 
 const usersRoute = Router();
 
-const users = [
+let users = [
   { id: 1, name: "Euclides" },
   { id: 2, name: "John Doe" },
   { id: 3, name: "Talitah" },
@@ -13,13 +13,19 @@ type GetUserType = {
   userId: string;
 };
 
+type CreateUserType = {
+  name: string;
+};
+
 // GET /users
 usersRoute.get("/users", (req: Request, res: Response, next: Next) => {
   res.status(statusCode.OK).send({ users });
 });
 
 // GET /users/:userId
-usersRoute.get("/users/:userId", (req: Request<GetUserType>, res: Response, next: Next) => {
+usersRoute.get(
+  "/users/:userId",
+  (req: Request<GetUserType>, res: Response, next: Next) => {
     const { userId } = req.params;
     const user = users.filter((user) => user.id === parseInt(userId));
 
@@ -28,6 +34,20 @@ usersRoute.get("/users/:userId", (req: Request<GetUserType>, res: Response, next
 );
 
 // POST /users
+usersRoute.post(
+  "/users",
+  (req: Request<CreateUserType>, res: Response, next: Next) => {
+    const name = req.body?.name;
+    const user = { id: users.length + 1, name };
+
+    users.push(user);
+
+    // console.log(req)
+
+    res.status(statusCode.CREATED).send({ user });
+  }
+);
+
 // PUT /users/:userId
 // PUT /users/:userId
 // DELETE /users/:userId
