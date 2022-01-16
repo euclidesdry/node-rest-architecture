bfea424ae99740bbc4a6489ab9c75fe33eed1870
euclidesdry <euclidesdry@gmail.com>
2022-01-16 15:13:46 +0100

    Getting Users By Query Params, userId 🧑‍🦲

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,15 +1,31 @@
-import { Router, Request, Response, NextFunction as Next } from 'express';
+import { Router, Request, Response, NextFunction as Next } from "express";
 
 const usersRoute = Router();
 
-// GET /users
-usersRoute.get('/users', (req: Request, res: Response, next: Next) => {
-  const users = [{ name: 'Euclides' }];
+const users = [
+  { id: 1, name: "Euclides" },
+  { id: 2, name: "John Doe" },
+  { id: 3, name: "Talitah" },
+];
 
-  res.status(200).send({users});
+type GetUserType = {
+  userId: string;
+};
+
+// GET /users
+usersRoute.get("/users", (req: Request, res: Response, next: Next) => {
+  res.status(200).send({ users });
 });
 
 // GET /users/:userId
+usersRoute.get("/users/:userId", (req: Request<GetUserType>, res: Response, next: Next) => {
+    const { userId } = req.params;
+    const user = users.filter((user) => user.id === parseInt(userId));
+
+    res.status(200).send({ user });
+  }
+);
+
 // POST /users
 // PUT /users/:userId
 // PUT /users/:userId
