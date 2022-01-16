e47d686ef9a882569e2709ad1dfca9437b2abafa
euclidesdry <euclidesdry@gmail.com>
2022-01-16 12:29:38 +0100

    Starting with ExpressJS + Typescript 🛵 🖐️

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Starting Node Server by Euclides DryC`s Machine 🥕');
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res
+    .status(200)
+    .send({ app: "Rest Arch", version: "0.0.1", author: "Euclides" });
+});
+
+app.get("/status", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ status: "OK" });
+});
+
+app.listen(4060, () => {
+  console.log("Starting Node Server by Euclides DryC`s Machine 🥕");
+});
